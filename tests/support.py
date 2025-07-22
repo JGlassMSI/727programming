1a26293a8b0ba4e7e63510df5d9c4b6bc66a43fb
Jeff Glass <glass.jeffrey@gmail.com>
2025-07-22 09:09:43 -0500

    More work on OCR - trying with recognizing known pre-saved images

--- tests/support.py
+++ tests/support.py
@@ -1,11 +1,12 @@
 import csv
+from itertools import chain
 from pathlib import Path
 from textwrap import dedent
 import os
 import uuid
 from typing import Generator
 
-from PIL import Image
+from PIL import Image, ImageOps
 from pyscreeze import _locateAll_pillow, locate
 import pytesseract
 import pyautogui
@@ -26,7 +27,7 @@ class NoMatchingImageException(pyautogui.ImageNotFoundException):
 def normalize_tag_name(tagname: str | None) -> str | None:
     #TODO make this better
     if not tagname: return tagname
-    return (tagname.strip(" |_\n")
+    return (tagname.strip(" |\n").rstrip("_")
             .replace(" ", "")
             .replace("|", "")
             .replace("\n", "")
@@ -47,19 +48,37 @@ def dump_user_words_to_file(data_file: str | Path = CSV_PATH, outfile = WORDS_PA
         for line in generate_user_words_from_file(data_file):
             f.write(line + "\n")
 
-def robust_image_to_string(img: Path | str | Image.Image, left_steps = 10, resize = 1, use_words = False, dump = False) -> str | None:
+def robust_image_to_string(img: Path | str | Image.Image, left_steps=1, resize = 1, use_words = False, dump = False) -> str | None:
     if not isinstance(img, Image.Image):
         img = Image.open(str(img))
 
-    
+    # TODO This is the experiemntal filters section
+    thresh = 127
+    fn = lambda x : 255 if x > thresh else 0
+    img = img.convert("L")
     if resize != 1:
         size = img.size
-        img = img.resize((size[0]*resize, size[1]*resize), Image.Resampling.LANCZOS)
+        resized_image = img.resize((size[0]*resize, size[1]*resize), Image.Resampling.LANCZOS)
+    else:
+        resized_image = img
     
     for _ in range(left_steps):
-        if text := pytesseract.image_to_string(img, config='--psm 7 --oem 3' +  f' --user-words {WORDS_PATH.absolute()}' if use_words else ''): return text
-        img = img.crop((.1, 0, img.width, img.height))
-    if dump: img.save(f".out/{uuid.uuid4()}.png")
+        if text := pytesseract.image_to_string(resized_image, config='--psm 7 --oem 3 ' +  f' --user-words {WORDS_PATH.absolute()} -c load_system_dawg=F -c load_freq_dawg=F' if use_words else ''): return text
+        resized_image = resized_image.crop((.1, 0, resized_image.width, resized_image.height))
+
+    #If OCR fails, try looking up by pre-saved image in original image
+    for obj in chain(os.listdir(IMAGES / "tagname_images"), os.listdir(IMAGES / "value_images")):
+        full_path = str(IMAGES / "tagname_images" / obj)
+        if os.path.isfile(full_path):
+            match_image = Image.open(full_path)
+            #print(f"{match_image.size=} {img.size=}")
+            if all(match_image.size[i] <= img.size[i] for i in (0, 1)):
+                print(f"Checking {full_path} in image")
+                if locate(full_path, img):
+                    print(f"Found in image for {Path(full_path).stem}!")
+                    return Path(full_path).stem
+
+    if dump: resized_image.save(f".out/{uuid.uuid4()}.png")
     return None
 
 
@@ -112,7 +131,7 @@ def get_data_from_dataview(img: Path | str | Image.Image) -> dict[str, int | str
     ROW_HEIGHT = 16
     ROW_HEIGHT_PAD = 3
 
-    VALUE_LEFT = ROW_LEFT + 308
+    VALUE_LEFT = ROW_LEFT + 313
     VALUE_RIGHT = VALUE_LEFT + 57
 
     values: dict[str, int | str | None] = {}
@@ -121,23 +140,28 @@ def get_data_from_dataview(img: Path | str | Image.Image) -> dict[str, int | str
     while True:
         row_top = TOP_OF_ROWS + row_index * ROW_HEIGHT
         tagname_image = img.crop((ROW_LEFT, row_top, ROW_RIGHT, row_top + ROW_HEIGHT + ROW_HEIGHT_PAD))
-        tagname = normalize_tag_name(robust_image_to_string(tagname_image, resize=3, use_words=True, dump=True))
+        tagname = normalize_tag_name(robust_image_to_string(tagname_image, resize=3, use_words=True))
         if not tagname:
             tagname_image.save(".out/first_failed_tagname_lookup.png")
             break
+        if row_index > 30: break #DEBUG
 
         value_image = img.crop((VALUE_LEFT, row_top, VALUE_RIGHT, row_top + ROW_HEIGHT + ROW_HEIGHT_PAD))
 
         unchecked_box = locate(str(IMAGES / "checkbox_empty.png"), value_image)
         checked_box = locate(str(IMAGES / "checkbox_checked.png"), value_image)
 
-        if checked_box and not unchecked_box: value = 1
-        elif unchecked_box and not checked_box: value = 0
+        if checked_box and not unchecked_box: value = True
+        elif unchecked_box and not checked_box: value = False
         elif checked_box and unchecked_box: raise ValueError(f"Box for tagname {tagname} was detected as both checked and not checked")
         else:
-            value = robust_image_to_string(value_image, resize=4, left_steps=1)
-            print(f"Initial OCR: {tagname!r}:{value}")
+            value = robust_image_to_string(value_image, resize=3, left_steps=5, dump=True)
 
+        _og_value = value
+        try:
+            value = int(str(value).strip())
+        except ValueError as err:
+            value = _og_value
         values[tagname] = value
         row_index += 1
         
--- tests/test_tesseract.py
+++ tests/test_tesseract.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from pathlib import Path
 
 import pyscreeze
@@ -25,6 +26,39 @@ def test_tesseract_on_dataview_rows():
     assert_image_contains_image(IMAGES / 'dataview_line_bool_unchecked.png', IMAGES / "checkbox_empty.png")
 
 def test_getting_dataview_data():
-    data = get_data_from_dataview(IMAGES / "dataview_3.png")
-    print(data)
-    assert False
+    data = get_data_from_dataview(IMAGES / "dataview_2.png")
+    expected_data = {
+        "MainWheel_Run"                         : True,
+        "Drive_MainWheel_Write.RunFrequency"    : 15,
+        "Drive_MainWheel_Write.JogFrequency"    : 0,
+        "Drive_MainWheel_Write.Timeout"         : True,
+        "Drive_MainWheel_Write.Error"           : True,
+        "Drive_MainWheel_Write.Success"         : True,
+        "Drive_MainWheel_Write.Complete"        : False,
+        "Drive_MainWheel_Write.InProgress"      : False,
+        "Drive_MainWheel_Write.ExcResponse"     : None,
+        "MainWheel_Direction"                   : 9999,
+        "MainWheel_FaultReset"                  : 0,
+        "MainWheel_Jog"                         : False,
+        "MainWheel_RunModeData"                 : False,
+        "MainWheel_StopModeData"                : False,
+        "NoseGear_Request_Down_Ext"             : False,
+        "NoseGear_Request_Up_Ext"               : False,
+        "_NoseGear_Up_Request"                  : False,
+    }
+
+    formatstring = "{key: <50} : {found: <50}"
+
+    print(formatstring.format(key="Expected Tagnames", found="Found Tagnames"))
+    print(formatstring.format(key="-----------------", found="--------------"))
+    for expected_key, ocr_key in zip_longest(sorted(expected_data.keys()), sorted(data.keys())):
+        print(formatstring.format(key=str(expected_key), found=str(ocr_key)))
+    print("\n\n")
+
+    formatstring = "{key: <50} : {found: <30} / {expected: <30}"
+    print(formatstring.format(key = "Tag Name", found = "Ocr Data", expected = "Expected Value"))
+    print(formatstring.format(key = "--------", found = "--------", expected = "--------------"))
+    for k, v in data.items():
+        print(formatstring.format(key = str(k), found = str(v), expected = str(expected_data[k])))
+    for k, v in data.items():
+        assert data[k] == v, f"OCR found {str(k) + ":" + str(data[k])} but it should have been {str(v)}"
